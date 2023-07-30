765f499bc473e0a7a42d8a4c026d20a2aeb06375
Chase Hard <dchasehardin@gmail.com>
2023-07-30 18:48:02 -0500

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-zinc-800">
+      <div className="flex flex-col gap-1 items-center">
+        <div className="text-8xl">🧰</div>
+        <h1 className="text-5xl font-bold text-zinc-400">developer.toolbox</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
